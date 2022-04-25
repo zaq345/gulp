540a5557059fc0c089dc8f29161f48f5e72c9d90
Юлия Ситникова <yu.sitnikova@kokoc.tech>
2022-04-25 22:31:24 +0500

    Добавляет начальное состояние репозитория

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,34 @@
-function defaultTask(cb) {
-    // Здесь будет код вашей задачи, выполняемой по умолчанию
-    cb();
-  }
-  
-exports.default = defaultTask
+const gulp = require("gulp");
+const del = require("del");
+const browserSync = require('browser-sync').create();
+
+/**
+ * Очищаем папку dist
+ */
+const clean = () => {
+  return del("build");
+};
+
+/**
+ * Копируем все содержимое из папки src в dist
+ */
+const copy = () => {
+  return gulp.src('src/**/*.*')
+    .pipe(gulp.dest('build'))
+};
+
+/**
+ * Запускаем сервер
+ */
+
+const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
+
+  gulp.watch('src/**/*').on('all', gulp.series(copy, browserSync.reload));
+};
+
+exports.default = gulp.series(clean, copy, server);
