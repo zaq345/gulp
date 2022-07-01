d200465a488853a569518e2001a0fbcbaf881472
Алексей Дитяткин <alexdit118@gmail.com>
2022-07-02 00:11:51 +0300

    v8

--- src/js/frame4.js
+++ src/js/frame4.js
@@ -10,30 +10,13 @@ $(document).ready(function(){
   $(".frame4__menu-item").on('click', function(){
     var filter = $(this).data('filter');
     $(".frame4__slider").slick('slickUnfilter');
-
-    // console.log("click-event, filter = "+filter);
     
     if(filter == 'Male' || 'Female' || 'Kids'){
-      // console.log("Male click-event");
-      // $(".frame4__slider").slick('slickFilter','.frame4__slider-card_Male');
       $(".frame4__slider").slick('slickFilter', function() {
         return $('[data-filter="'+filter+'"]', this).length === 1;
       })
     }
-    // else if(filter == 'Female'){
-    //   // console.log("Female click-event");
-    //   $(".frame4__slider").slick('slickFilter',function() {
-    //     return $('[data-filter="'+filter+'"]', this).length === 1;
-    //   })
-    // }
-    // else if(filter == 'Kids'){
-    //   // console.log("Kids click-event");
-    //   $(".frame4__slider").slick('slickFilter',function() {
-    //     return $('[data-filter="'+filter+'"]', this).length === 1;
-    //   })
-    // }
-    /*else*/ if(filter == 'All'){
-      // console.log("All click-event");
+    if(filter == 'All'){
       $(".frame4__slider").slick('slickUnfilter');
     }
     
--- src/js/frame5.js
+++ src/js/frame5.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $('.frame5__slider').slick({
     dots: 0,
+    fade: true,
+    cssEase: 'linear',
     prevArrow: $('.frame5__slider-prev-btn'),
     nextArrow: $('.frame5__slider-next-btn'),
   });
